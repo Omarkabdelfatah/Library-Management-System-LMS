d5e5b859d0fce1705a4f28131f03f4e9abd0d35e
Omarkabdelfatah <omar1234u@gmail.com>
2024-11-17 22:43:07 +0200

    Initial project setup

---
+++ src/main/java/com/example/Library_Management_System_LMS/LibraryManagementSystemLmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Library_Management_System_LMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementSystemLmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementSystemLmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Library_Management_System_LMS/LibraryManagementSystemLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Library_Management_System_LMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemLmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
